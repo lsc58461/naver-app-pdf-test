b40cbd529e5b91844f1ed7604d875b604199429a
이정윤 <lsc58461@gmail.com>
2025-02-10 12:35:35 +0900

    feat: PDF 생성 오류 시 사용자에게 알림 표시

--- app/api/pdf/route.tsx
+++ app/api/pdf/route.tsx
@@ -2,6 +2,7 @@ import { renderToStream } from "@react-pdf/renderer";
 import { NextResponse } from "next/server";
 
 import { ResumePDFTemplate } from "@/components/pdf-template";
+import { toast } from "react-toastify";
 
 export async function GET() {
   try {
@@ -27,6 +28,7 @@ export async function GET() {
     });
   } catch (error) {
     console.error("PDF 생성 중 오류:", error);
+    toast.error(`PDF 생성 중 오류가 발생했습니다: ${error}`);
     return NextResponse.json({ error: "PDF 생성 실패" }, { status: 500 });
   }
 }
